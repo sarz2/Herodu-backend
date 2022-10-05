1394eeecc976a53dc669da836ac70fed9602b3b7
sarz2 <sara.abou@chasacademy.se>
2022-10-05 15:35:24 +0200

    auth and item routes added

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const authController = require("../controllers/authControllers");
+const router = Route();
+const auth = require("../middleware/auth");
+
+router.post("/register", authController.signup);
+router.post("/login", authController.login);
+router.get("/user", auth, authController.get_user);
---
+++ routes/item.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const itemController = require("../controllers/itemController");
+const router = Router();
+
+router.get("/items", itemController.get_items);
+router.post("/items", itemController.post_item);
+router.put("/items:id", itemController.update_item);
+router.delete("/items/:id", itemController.delete_item);
