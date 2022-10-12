c9492aeae7aef8be6e402765097bd9c6897b1261
sarz2 <sara.abou@chasacademy.se>
2022-10-12 17:57:18 +0200

    connect db

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(
+      process.env.MONGO_CONNECTION_STRING
+    );
+
+    console.log(`MongoDB connected ${connection.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
