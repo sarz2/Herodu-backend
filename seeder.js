40369a23c3845ac514244fd04ef11ecc60fa57bf
sarz2 <sara.abou@chasacademy.se>
2022-10-12 18:54:45 +0200

    make seeder for db

---
+++ seeder.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ path: `${__dirname}/.env` });
+const users = require("./data/users");
+const products = require("./data/products");
+const User = require("./models/userModel");
+const Product = require("./models/productModel");
+const Order = require("./models/orderModel");
+const connectDb = require("./database/db");
+
+connectDb();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+
+    const seederProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(seederProducts);
+
+    console.log("Data imported");
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log("Data destroyed");
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
