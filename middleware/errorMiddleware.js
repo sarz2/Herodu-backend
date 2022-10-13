1eaa134c289814e94062f9e7cbe942a1b1b4c111
sarz2 <sara.abou@chasacademy.se>
2022-10-13 17:23:40 +0200

    create error middleware

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,22 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode;
+
+  if (statusCode === 200) {
+    statusCode = 500;
+  } else {
+    statusCode = statusCode;
+  }
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = { notFound, errorHandler };
