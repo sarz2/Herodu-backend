f9b0201fa90637a4d2776ec17f9c20c8dade5053
sarz2 <sara.abou@chasacademy.se>
2022-10-20 21:18:10 +0200

    create routes for user

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const userController = require("../controllers/userController");
+const { protectedRoute } = require("../middleware/authMiddleware");
+const router = Router();
+
+router.post("/login", userController.login);
+router.route("/profile").get(protectedRoute, userController.getUserProfile);
+
+module.exports = router;
