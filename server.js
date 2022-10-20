a08d147e63fd0b4e230e974f257b24511005572d
sarz2 <sara.abou@chasacademy.se>
2022-10-20 21:20:15 +0200

    add user routes

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const connectDB = require("./database/db");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
