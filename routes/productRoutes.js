ef995eabe22663d37900bc35ac656a373149c20e
sarz2 <sara.abou@chasacademy.se>
2022-11-07 12:04:22 +0100

    Add route for searching for product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,7 @@ const { protectedRoute } = require("../middleware/authMiddleware");
 const router = Router();
 
 router.route("/").get(productController.getProducts);
+router.route("/search/:keyword").get(productController.getProducts);
 router.route("/:id").get(productController.getOneProduct);
 router
   .route("/:id/reviews")
