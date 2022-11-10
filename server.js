ea71ee47ed835948c882baf0459970bc75c15e98
sarz2 <sara.abou@chasacademy.se>
2022-11-10 22:00:28 +0100

    Create routes for production

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 require("dotenv").config({ path: `${__dirname}/.env` });
 const morgan = require("morgan");
@@ -18,12 +19,22 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(express.urlencoded()); //Could be redundant, will try without this
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/herodu-frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "herodu-frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 app.use(notFound);
 
