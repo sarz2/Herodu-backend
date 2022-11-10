a2e00e53b495ddb59bfc804382072e9280a1db92
sarz2 <sara.abou@chasacademy.se>
2022-11-10 22:02:11 +0100

    Delete unused files

--- routes/item.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const itemController = require("../controllers/itemController");
-const router = Router();
-
-router.get("/items", itemController.get_items);
-router.post("/items", itemController.post_item);
-router.put("/items:id", itemController.update_item);
-router.delete("/items/:id", itemController.delete_item);
