7d5236209b3d962a36c44c18edda0d15421edb92
sarz2 <sara.abou@chasacademy.se>
2022-11-13 21:19:42 +0100

    Remove unused file

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const authController = require("../controllers/authController");
-const router = Route();
-const auth = require("../middleware/auth");
-
-router.post("/register", authController.signup);
-router.post("/login", authController.login);
-router.get("/user", auth, authController.get_user);
